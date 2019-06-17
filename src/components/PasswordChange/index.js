66e463713e9d7bc294a792c3f1133190a42d7dfb
Jeff Webb <webbwear@icloud.com>
2019-06-17 18:43:14 -0400

    created basic React component for each index file

---
+++ src/components/PasswordChange/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PassChange = () => (
+  <div>
+    <h1>PassChange</h1>
+  </div>
+);
+
+export default PassChange;
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PassForget = () => (
+  <div>
+    <h1>PassForget</h1>
+  </div>
+);
+
+export default PassForget;
