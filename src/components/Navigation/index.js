5bfc33c5fd96c5dab3f5e2ccc8157bca9346de66
Jeff Webb <webbwear@icloud.com>
2019-06-17 20:12:04 -0400

    navigation routes and links

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Nav = () => (
+import * as ROUTES from '../../constants/routes';
+
+const Navigation = () => (
   <div>
-    <h1>Nav</h1>
+    <ul>
+      <li>
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.LANDING}>Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.HOME}>Home</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ACCOUNT}>Account</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ADMIN}>Admin</Link>
+      </li>
+    </ul>
   </div>
 );
 
-export default Nav;
+export default Navigation;
